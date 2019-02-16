723c79f2a87380a98a95581fbfb72202815bee58
devfuner@gmail.com <No411gosu>
2019-02-16 15:23:23 +0900

    정규표현식으로 패스워드 체크

--- public/javascripts/password-checker.js
+++ public/javascripts/password-checker.js
@@ -1,9 +1,36 @@
+const defaultPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#$%^&*()\-=_+[\]{};:<>?,.]).{8,}/;
+
 module.exports = {
+
+    createPatternString(option) {
+        let patternString = '';
+        if (option) {
+            if (option.numeric) {
+                patternString += '(?=.*\\d)';
+            }
+            if (option.lowercase) {
+                patternString += '(?=.*[a-z])';
+            }
+            if (option.uppercase) {
+                patternString += '(?=.*[A-Z])';
+            }
+            if (option.specialcharacter) {
+                patternString += '(?=.*[~!@#$%^&*()\\-=_+[\\]{};:<>?,.])';
+            }
+            patternString += '.{8,}';
+        }
+
+        return patternString;
+    },
+
     /**
      * input password string
      * output strong level boolean
      */
-    passwordChecker(password) {
-        return false;
+    passwordChecker(password, option) {
+        const patternString = this.createPatternString(option);
+        const pattern = patternString ? new RegExp(patternString) : defaultPattern;
+
+        return pattern.test(password);
     }
 }
--- test/password-checker.spec.js
+++ test/password-checker.spec.js
@@ -3,15 +3,30 @@ var assert = require('chai').assert;
 
 describe('Password Checker', function() {
     var tests = [
-        {args: 'abcdef', expected: false},
-        {args: 'abc123', expected: false},
-        {args: 'abc123!@#', expected: true}
+        {args: 'abcdefghijk', expected: false},
+        {args: 'abcDEFghijk', expected: false},
+        {args: 'abcDEF12345', expected: false},
+        {args: 'abcDE1&', expected: false},
+        {args: 'abcDE1&.', expected: true},
+        {args: 'abc12345', expected: false},
+        {args: 'abcDE12F', expected: false},
+        {args: '123abcDEF%', expected: true},
+        {args: '_abcDEF123%', expected: true},
+        {args: 'abcDEF123!@#', expected: true},
+        {args: 'abcDEF123%', expected: true},
     ];
+
+    const option = {
+        lowercase: true,
+        uppercase: true,
+        numeric: true,
+        specialcharacter: true
+    }
 
     tests.forEach(function(test) {
         it('is Strong? ' + test.args, function() {
-            var isString = pc.passwordChecker(test.args);
-            assert.equal(isString, test.expected);
+            var isStrong = pc.passwordChecker(test.args);
+            assert.equal(isStrong, test.expected);
         });
     });
 });
