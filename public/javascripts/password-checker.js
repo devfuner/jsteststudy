132aafa1c7eb085df2a206fb98d18881972dfa47
devfuner@gmail.com <No411gosu>
2019-02-17 14:23:13 +0900

    패스워드 체크 기능 추가
    - 연속된 문자 체크
    - 같은 문자 연속 사용 체크
    - 배열 분할 기능 추가
    - 소문자 포함 체크
    - 대문자 포함 체크
    - 특수문자 포함 체크
    - 숫자 포함 체크

--- public/javascripts/password-checker.js
+++ public/javascripts/password-checker.js
@@ -32,5 +32,111 @@ module.exports = {
         const pattern = patternString ? new RegExp(patternString) : defaultPattern;
 
         return pattern.test(password);
+    },
+
+    division(arr, n) {
+        if (n <= 0) {
+            return arr;
+        }
+
+        const len = arr.length;
+        const cnt = Math.floor(len / n) + (Math.floor(len % n) > 0 ? 1 : 0);
+        let temp = [];
+
+        // for (let i = 0; i < cnt; i++) {
+        //     temp.push(arr.splice(0, n));
+        // }
+
+        while (arr.length >= n) {
+            temp.push(arr.slice(0, n));
+            arr.shift();
+        }
+
+        return temp;
+    },
+
+    isIncludes(char, characters) {
+        return characters.includes(char);
+    },
+
+    isLowercase(c) {
+        return this.isIncludes(c, 'abcdefghijklmnopqrstuvwxyz');
+    },
+
+    isUppercase(c) {
+        return this.isIncludes(c, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    },
+
+    isNumeric(c) {
+        return this.isIncludes(c, '0123456789');
+    },
+
+    isSpecialCharacters(c) {
+        return this.isIncludes(c, '~!@#$%^&*()-=_+[]{};:<>?,.');
+    },
+
+    isSameCharRepeat(characters, count=3) {
+        const arr = this.division(characters.split(''), count);
+        return arr.some((ea) => ea.every((e, _, a) => {
+            if (a.length === 1) {
+                return false;
+            }
+
+            return e === a[0]
+        }));
+    },
+
+    isSequenceCharacters (characters, count=3) {
+        const arr = this.division(characters.split(''), count);
+        return arr.some((ea) => {
+            const sequenceChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+            const idx = sequenceChar.indexOf(ea[0]);
+
+            if (idx === -1) {
+                return false;
+            }
+
+            for (let i = 0; i < ea.length; i++) {
+                let j = i + idx;
+                if (!(ea[i] === sequenceChar[j])) return false;
+            }
+
+            return true;
+        });
+    },
+
+    passwordChecker2(password, option={ lengthMin: 8, lengthMax: 99}) {
+        const chars = password.split('');
+
+        if (password.length < option.lengthMin
+            || password.length > option.lengthMax) {
+            return false;
+        }
+
+        // 소문자
+        let result = chars.some((c) => this.isLowercase(c));
+        if (!result) return result;
+
+        // 대문자
+        result = chars.some((c) => this.isUppercase(c));
+        if (!result) return result;
+
+        // 숫자
+        result = chars.some((c) => this.isNumeric(c));
+        if (!result) return result;
+
+        // 특수문자
+        result = chars.some((c) => this.isSpecialCharacters(c));
+        if (!result) return result;
+
+        // 같은 문자 반복 사용
+        result = chars.some((c) => this.isSameCharRepeat(c));
+        if (!result) return result;
+
+        // 연속된 문자 사용
+        result = chars.some((c) => this.isSequenceCharacters(c));
+        if (!result) return result;
+
+        return true;
     }
 }
--- test/password-checker.spec.js
+++ test/password-checker.spec.js
@@ -1,32 +1,105 @@
 const pc = require('../public/javascripts/password-checker.js');
 var assert = require('chai').assert;
 
-describe('Password Checker', function() {
-    var tests = [
-        {args: 'abcdefghijk', expected: false},
-        {args: 'abcDEFghijk', expected: false},
-        {args: 'abcDEF12345', expected: false},
-        {args: 'abcDE1&', expected: false},
-        {args: 'abcDE1&.', expected: true},
-        {args: 'abc12345', expected: false},
-        {args: 'abcDE12F', expected: false},
-        {args: '123abcDEF%', expected: true},
-        {args: '_abcDEF123%', expected: true},
-        {args: 'abcDEF123!@#', expected: true},
-        {args: 'abcDEF123%', expected: true},
-    ];
+describe('[패스워드 체크]', function () {
 
-    const option = {
-        lowercase: true,
-        uppercase: true,
-        numeric: true,
-        specialcharacter: true
-    }
+    describe('정규식으로 패스워드 체크', function () {
+        var tests = [
+            {args: 'abcdefghijk', expected: false},
+            {args: 'abcDEFghijk', expected: false},
+            {args: 'abcDEF12345', expected: false},
+            {args: 'abcDE1&', expected: false},
+            {args: 'abc12345', expected: false},
+            {args: 'abcDE12F', expected: false},
+            {args: 'aaaDe122F%', expected: true},
+            {args: 'abcDE1&.', expected: true},
+            {args: '123abcDEF%', expected: true},
+            {args: '_abcDEF123%', expected: true},
+            {args: 'abcDEF123!@#', expected: true},
+            {args: 'abcDEF123%', expected: true},
+        ];
 
-    tests.forEach(function(test) {
-        it('is Strong? ' + test.args, function() {
-            var isStrong = pc.passwordChecker(test.args);
-            assert.equal(isStrong, test.expected);
+        const option = {
+            lowercase: true,
+            uppercase: true,
+            numeric: true,
+            specialcharacter: true
+        };
+
+        tests.forEach(function (test) {
+            it(test.args + '는 조건에 맞나요?', function () {
+                const isStrong = pc.passwordChecker(test.args, option);
+                assert.equal(isStrong, test.expected);
+            });
+        });
+    });
+
+    describe('패스워드 체크 ', function () {
+        var tests = [
+            {args: 'abcdefghijk', expected: false},
+            {args: 'abcDEFghijk', expected: false},
+            {args: 'abcDEF12345', expected: false},
+            {args: 'abcDE1&', expected: false},
+            {args: 'abc12345', expected: false},
+            {args: 'abcDE12F', expected: false},
+            {args: 'aaaDe122F%', expected: false},
+            {args: 'abcDE1&.', expected: false},
+            {args: '123abcDEF%', expected: false},
+            {args: '_abcDEF123%', expected: false},
+            {args: 'abcDEF123!@#', expected: false},
+            {args: 'abcDEF123%', expected: false},
+        ];
+
+        tests.forEach(function (test) {
+            it(test.args + '는 조건에 맞나요?', function () {
+                const isStrong = pc.passwordChecker2(test.args);
+                assert.equal(isStrong, test.expected);
+            });
+        });
+    });
+
+    describe('배열 분할', function () {
+        it('2개로 나눠지나요?', function () {
+            const arr = pc.division([1, 2, 3, 4, 5, 6, 7], 2);
+
+            console.log('arr >', arr);
+            assert.equal(arr.length, 6);
+        });
+    });
+
+    describe('같은 문자가 연속하면', function () {
+        var tests = [
+            {args: 'aaa123!@#', repeat: 3, expected: true},
+            {args: 'abc111!@#', repeat: 3, expected: true},
+            {args: 'abc123!!!', repeat: 3, expected: true},
+            {args: 'aaaaa123!@#', repeat: 5, expected: true},
+            {args: 'abc11111!@#', repeat: 5, expected: true},
+            {args: 'abc123!!!!!', repeat: 5, expected: true},
+            {args: 'abc123!@#', repeat: 3, expected: false},
+            {args: 'abc123!@#$%', repeat: 5, expected: false},
+        ];
+
+        tests.forEach(function (test) {
+            it('안되요! ' + test.args, function () {
+                const result = pc.isSameCharRepeat(test.args, test.repeat);
+                assert.equal(result, test.expected);
+            });
+        });
+    });
+
+    describe('연속문자가 3자리 이상이면', function () {
+        var tests = [
+            {args: 'aaa123!@#', repeat: 3, expected: true},
+            {args: 'abc111!@#', repeat: 3, expected: true},
+            {args: 'abc123xyz', repeat: 3, expected: true},
+            {args: 'zyx321*&^', repeat: 3, expected: false},
+        ];
+
+        tests.forEach(function (test) {
+            it('안되요! ' + test.args, function () {
+                const result = pc.isSequenceCharacters(test.args, test.repeat);
+                assert.equal(result, test.expected);
+            });
         });
     });
 });
